e86aee66675be469b8deb3d9531de7f73430f61c
CAIO BRUNO S M OLIVEIRA <115943715+CaioBrun0@users.noreply.github.com>
2023-06-08 13:47:06 -0300

    Projeto na fase inicial, funcionando.

---
+++ src/Controller/MainController.java
@@ -0,0 +1,19 @@
+package Controller;
+
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
